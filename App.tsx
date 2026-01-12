8c744340670f59c465385ddddca302dd8381636e
Moises <moisesdaipb@gmail.com>
2026-01-12 00:06:42 +0000

    Corrigido bug de renderização em App.tsx

--- App.tsx
+++ App.tsx
@@ -14,6 +14,8 @@ import Navbar from './components/Navbar';
 import AuthGuard from './components/AuthGuard';
 
 
+
+
 const AppContent: React.FC = () => {
   const location = useLocation();
   const hideNavbarPaths = ['/', '/editor', '/shopping', '/summary', '/share'];
